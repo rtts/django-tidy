fff6bdcf657c11f3b6b2ed52f7f60a392e43f9db
Jaap Joris Vens <jj@rtts.eu>
2021-01-31 18:28:12 +0100

    Nice one!

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     packages = find_packages(),
     classifiers = [
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)'
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Operating System :: OS Independent',
     ],
     python_requires = '>=3.8',
